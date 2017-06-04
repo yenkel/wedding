5dc627434149cd2dd111ad3d4dcf10b4880c570f
gab1306 <gabmimouni@gmail.com>
2017-06-04 16:09:06 +0300

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var mongoose = require('mongoose');
 path = require('path');
 
 
